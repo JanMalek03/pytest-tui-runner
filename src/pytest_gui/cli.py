c2e2b3efaf183fdc5d62f064b768d3c086046a67
JanMalek03 <176jenda@gmail.com>
2025-08-18 13:30:00 +0200

    Add terminal command to run application

--- src/pytest_gui/cli.py
+++ src/pytest_gui/cli.py
@@ -1,12 +1,23 @@
+import subprocess
+import sys
+
 import click
+
+from pytest_gui.logs.logger_config import logger
 
 
 @click.group()
-def cli():
-    """CLI pro pytest-gui plugin."""
+def cli() -> None:
+    """CLI for pytest-gui plugin."""
 
 
 @cli.command()
-def run():
-    """Spustí TUI."""
-    print("Spouštím TUI...")
+def run() -> None:
+    """Run the terminal application."""
+    try:
+        logger.info("Starting the application...")
+        subprocess.run(["uv", "run", "-m", "src.pytest_gui.ui.tui.app"], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error launching the application: {e}")
+        logger.error(f"Error launching the application: {e}")
+        sys.exit(1)
