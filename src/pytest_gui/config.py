3faccc2d184ef342718007f4999fa9ad05e0ab0d
JanMalek03 <176jenda@gmail.com>
2025-09-22 11:38:09 +0200

    Add cache for loading configuration files

--- src/pytest_gui/config.py
+++ src/pytest_gui/config.py
@@ -1,4 +1,5 @@
 import json
+from functools import lru_cache
 from pathlib import Path
 
 import yaml
@@ -7,27 +8,12 @@ from pytest_gui.logging import logger
 from pytest_gui.utils.types.config import TestConfig
 
 
+@lru_cache(maxsize=1)
 def load_config(file_path: str) -> TestConfig:
-    # TODO: add json as config. Now there is no json file to be loaded
-    """Load and parses a configuration file.
+    """Load and parse a configuration file.
 
-    Parameters
-    ----------
-    file_path : str
-        The path to the configuration file.
-
-    Returns
-    -------
-    TestConfig
-        An object representing the parsed test configuration.
-
-    Raises
-    ------
-    FileNotFoundError
-        If the configuration file does not exist.
-    ValueError
-        If the file format is not YAML or JSON.
-
+    The result is cached, so repeated calls with the same file_path return the same object
+    without re-reading the file.
     """
     path = Path(file_path)
 
@@ -38,11 +24,11 @@ def load_config(file_path: str) -> TestConfig:
     logger.debug(f"Config file path set to: '{file_path}'")
 
     if path.suffix in {".yaml", ".yml"}:
-        logger.debug("Opening config file as a 'yaml' file")
+        logger.debug("Parsing as YAML")
         with Path.open(path, encoding="utf-8") as file:
             return yaml.safe_load(file)
     elif path.suffix == ".json":
-        logger.debug("Opening config file as a 'json' file")
+        logger.debug("Parsing as JSON")
         with Path.open(path, encoding="utf-8") as file:
             return json.load(file)
     else:
